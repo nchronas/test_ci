098caa03d633ac065b2920e5caf5dff999a16182
nchronas <nchronas@gmail.com>
2018-03-16 16:00:26 +0100

    First passing test

--- test/test_d.c
+++ test/test_d.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "d.h"
+#include "m.h"
 
 void setUp(void)
 {
@@ -9,7 +10,8 @@ void tearDown(void)
 {
 }
 
-void test_d_NeedToImplement(void)
+void test_d_test1(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement d");
+    TEST_ASSERT_EQUAL_HEX8(84, AverageThreeBytes(0, 126, 126));
+    //TEST_IGNORE_MESSAGE("Need to Implement d");
 }
