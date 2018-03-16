c42d1c81d58f82e22d1cef5b16a859eb19b65b6f
nchronas <nchronas@gmail.com>
2018-03-16 16:16:43 +0100

    Mocking function and testing

--- test/test_d.c
+++ test/test_d.c
@@ -1,6 +1,7 @@
 #include "unity.h"
 #include "d.h"
-#include "m.h"
+#include <stdbool.h>
+#include "mock_m.h"
 
 void setUp(void)
 {
@@ -12,6 +13,9 @@ void tearDown(void)
 
 void test_d_test1(void)
 {
+    mocked_ExpectAndReturn(false);
     TEST_ASSERT_EQUAL_HEX8(84, AverageThreeBytes(0, 126, 126));
+    mocked_ExpectAndReturn(true);
+    TEST_ASSERT_EQUAL(-1, AverageThreeBytes(0, 126, 126));
     //TEST_IGNORE_MESSAGE("Need to Implement d");
 }
